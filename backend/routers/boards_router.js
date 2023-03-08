68181b6bf366a1a2582787f8a972af3e1f524d73
jasond20 <jason.dai@mail.utoronto.ca>
2023-03-08 11:37:08 -0500

    Dev: Changed pieces_router to boards_router in router folder

--- backend/routers/boards_router.js
+++ backend/routers/boards_router.js
@@ -1,5 +1,49 @@
-// import { Router } from 'express';
-// import { isAuthenticated } from '../middleware/helpers';
+import { Router } from 'express';
+import { isAuthenticated } from '../middleware/helpers';
+import { Board } from '../models/boards';
+
+const boardRouter = Router();
+
+boardRouter.get('/', isAuthenticated, async (req, res) => {
+    Board.findAll()
+        .then(boards => {
+        res.json(boards);
+        });
+    }
+);
+
+boardRouter.get('/:id', isAuthenticated, async (req, res) => {
+    Board.findById(req.params.id)
+        .then(board => {
+        res.json(board);
+        });
+    }
+);
+
+boardRouter.post('/', isAuthenticated, async (req, res) => {
+    Board.create(req.body)
+        .then(board => {
+        res.json(board);
+        });
+    }
+);
+
+boardRouter.put('/:id', isAuthenticated, async (req, res) => {
+    Board.update(req.body, { where: { id: req.params.id } })
+        .then(board => {
+        res.json(board);
+        });
+    }
+);
+
+boardRouter.delete('/:id', isAuthenticated, async (req, res) => {
+    Board.destroy({ where: { id: req.params.id } })
+        .then(board => {
+        res.json(board);
+        });
+    }
+);
+
 // import { Pieces } from '/models/pieces.js';
 
 // const pieceRouter = Router();
