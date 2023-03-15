2011465f63933734149834ac64a169c92e7029d6
Kia Rahmati <keia.r.ahmati@gmail.com>
2023-03-15 14:30:09 -0400

    game fixing

--- frontend/src/app/pages/game/game.component.ts
+++ frontend/src/app/pages/game/game.component.ts
@@ -1,15 +1,127 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { ApiService } from "../../services/api.service";
 
 @Component({
-  selector: 'app-game',
-  templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  selector: "app-game",
+  templateUrl: "./game.component.html",
+  styleUrls: ["./game.component.scss"],
 })
 export class GameComponent implements OnInit {
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private apiService: ApiService
+  ) {}
 
-  constructor() { }
+  ngOnInit() {
+    //If getRooms doeesnt return a room, create room with id 1
+    this.apiService.getRooms().subscribe({
+      next: (data) => {
+        console.log(data);
+      },
 
-  ngOnInit(): void {
+      error: (err) => {
+        console.log("No rooms found");
+        this.apiService.addRoom("Alpha Room").subscribe(() => {});
+        this.apiService.createBoard(1).subscribe(() => {});
+      },
+    });
+
+    //Get the board for room 1
+    this.apiService.getBoard(1).subscribe((data) => {
+      console.log(data);
+    });
+
+    this.updateBoard();
+
+    //Set the player names
   }
 
+  pieceSelect(x: number, y: number) {
+    //add the selected class to the piece
+    let piece = document.querySelector(
+      `[data-x="${x}"][data-y="${y}"]`
+    ) as HTMLElement;
+    piece.classList.add("selected");
+
+    //remove all event listeners from <p> elements
+    let pieces = document.querySelectorAll("p");
+    pieces.forEach((piece) => {
+      piece.removeEventListener("click", () => {
+        this.pieceSelect(x, y);
+      });
+    });
+
+    //add a event listener to all td elements
+    let squares = document.querySelectorAll("td");
+    squares.forEach((square) => {
+      const squareX = square.getAttribute("data-row");
+      const squareY = square.getAttribute("data-col");
+      if (squareX === null || squareY === null) {
+        console.log("Square not found");
+        return;
+      }
+      square.addEventListener("click", () => {
+        this.makeMove(x, y, parseInt(squareX), parseInt(squareY));
+      });
+    });
+  }
+
+  makeMove(startx: number, starty: number, endx: number, endy: number) {
+    this.apiService
+      .makeMove(1, startx, starty, endx, endy)
+      .subscribe((data) => console.log(data));
+
+    //remove the selected class from the piece
+    let piece = document.querySelector(
+      `[data-x="${startx}"][data-y="${starty}"]`
+    ) as HTMLElement;
+    piece.classList.remove("selected");
+
+    //remove all event listeners from <td> elements
+    let squares = document.querySelectorAll("td");
+    squares.forEach((square) => {
+      square.removeEventListener("click", () => {
+        this.makeMove(startx, starty, endx, endy);
+      });
+    });
+
+    this.updateBoard();
+  }
+
+  updateBoard() {
+    this.apiService.getPieces(1).subscribe((data) => {
+      for (let i = 0; i < data.pieces.length; i++) {
+        const piece = data.pieces[i];
+        //find the square with the same x and y
+        let square = document.querySelector(
+          `[data-row="${piece.xpos}"][data-col="${piece.ypos}"]`
+        );
+        if (square === null) {
+          console.log("Square not found");
+          return;
+        }
+        const display = document.createElement("p");
+        //record the piece's x and y
+        display.setAttribute("data-x", piece.xpos.toString());
+        display.setAttribute("data-y", piece.ypos.toString());
+        //add pieceSelect as event listener for clicking to the piece
+        display.addEventListener("click", () => {
+          this.pieceSelect(piece.xpos, piece.ypos);
+        });
+        //add the piece's type as a class for display
+        display.classList.add(piece.type);
+        //add the piece's side as a class for display
+        if (piece.side == 0) {
+          display.classList.add("aTeam");
+        } else {
+          display.classList.add("bTeam");
+        }
+
+        square.appendChild(display);
+      }
+    });
+  }
 }
