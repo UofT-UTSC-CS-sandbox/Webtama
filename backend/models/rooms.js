bb6756b105e4af0664cb63f7a2350825b7215c13
Kia Rahmati <keia.r.ahmati@gmail.com>
2023-03-15 19:56:53 -0400

    changed room model

--- backend/models/rooms.js
+++ backend/models/rooms.js
@@ -1,6 +1,7 @@
 import { sequelize } from "../datasource.js";
 import { DataTypes } from "sequelize";
 import { User } from "./users.js";
+import { Board } from "./boards.js";
 
 export const Room = sequelize.define("Room", {
   name: {
@@ -17,6 +18,6 @@ export const Room = sequelize.define("Room", {
     allowNull: true,
   },
 });
-
+Room.hasOne(Board);
 // Room.belongsToMany(User, { through: "UserRoom" });
 // User.belongsToMany(Room, { through: "UserRoom" });
