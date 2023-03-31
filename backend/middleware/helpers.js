13d2a92663fd08420a8631459e05ec8d3f73d0e3
jasond20 <jason.dai@mail.utoronto.ca>
2023-03-31 13:46:18 -0400

    Added auth0 working middleware

--- backend/middleware/helpers.js
+++ backend/middleware/helpers.js
@@ -1,7 +1,17 @@
-export const isAuthenticated = function (req, res, next) {
-  if (!req.session.userId)
-    return res.status(401).json({
-      error: "You are not authenticated.",
-    });
-  next();
-};
+import { expressjwt as jwt } from 'express-jwt';
+import jwksRsa from 'jwks-rsa';
+
+export const isAuthenticated = jwt({
+  algorithms: ['RS256'],
+  audience: 'https://dev-0rubju8i61qqpmgv.us.auth0.com/api/v2/',
+  issuer: `https://dev-0rubju8i61qqpmgv.us.auth0.com/`,
+  secret: 'RXn0GvIfMUKVcW2WuOXMNazcuKuNFjLrKNOt2OkUFDRPPaBP_A4wYvhm_Ku_r4tY',
+  
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://dev-0rubju8i61qqpmgv.us.auth0.com/.well-known/jwks.json'
+  }),
+
+});
