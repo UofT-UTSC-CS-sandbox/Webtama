1a6c319f1fdba487471fcce79aaa0b4ba61347e2
Kia Rahmati <keia.r.ahmati@gmail.com>
2023-03-31 13:49:07 -0400

    feat matchmaking

--- backend/routers/users_router.js
+++ backend/routers/users_router.js
@@ -30,16 +30,6 @@ usersRouter.post("/signup", async (req, res) => {
   const salt = bcrypt.genSaltSync(10);
   user.password = bcrypt.hashSync(password, salt);
   */
-  sgMail.setApiKey(
-    "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"
-  );
-  const msg = {
-    to: user.email, // Change to your recipient
-    from: "jasoncndai@gmail.com", // Change to your verified sender
-    subject: "Sending with SendGrid is Fun",
-    text: "and easy to do anywhere, even with Node.js",
-    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-  };
   sgMail
     .send(msg)
     .then(() => {
