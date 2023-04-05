4f908968d106c366d94c1db3a26281135c307a27
jasond20 <jason.dai@mail.utoronto.ca>
2023-04-05 19:32:26 -0400

    Changed back to localhost for non proxy test

--- backend/app.js
+++ backend/app.js
@@ -18,8 +18,8 @@ app.use(bodyParser.json());
 
 app.use(express.static("static"));
 const corsOptions = {
-  // origin: "http://localhost:4200",
-  origin: "http://webtama.works",
+  origin: "http://localhost:4200",
+  // origin: "http://webtama.works",
   credentials: true,
 };
 app.use(cors(corsOptions));
@@ -77,8 +77,8 @@ app.use("/api/rooms", roomRouter);
 
 const io = new Server(httpServer, {
   cors: {
-    // origin: "http://localhost:4200",
-    origin: "http://webtama.works",
+    origin: "http://localhost:4200",
+    // origin: "http://webtama.works",
     methods: ["GET", "POST"],
   },
 });
@@ -135,5 +135,5 @@ io.on("connection", (socket) => {
 
 httpServer.listen(PORT, (err) => {
   if (err) console.log(err);
-  else console.log("HTTP server on http://webtama.works:%s", PORT);
+  else console.log("HTTP server on http://localhost:%s", PORT);
 });
